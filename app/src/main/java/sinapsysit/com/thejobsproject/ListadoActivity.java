0dc9c08ec92a10e5fc0402377c19036aee7a298b
jujomago <jujomago@gmail.com>
2015-10-11 19:23:10 -0400

    agregados los menus

--- app/src/main/java/sinapsysit/com/thejobsproject/ListadoActivity.java
+++ app/src/main/java/sinapsysit/com/thejobsproject/ListadoActivity.java
@@ -2,6 +2,9 @@ package sinapsysit.com.thejobsproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ListadoActivity extends AppCompatActivity {
 
@@ -10,4 +13,30 @@ public class ListadoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuapp, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Toast.makeText(this, "Primera Opcion", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_settings2:
+                Toast.makeText(this,"Segunda Opcion",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this,"Tercera Opcion",Toast.LENGTH_SHORT).show();
+
+        }
+
+        return true;
+    }
+
+
 }
