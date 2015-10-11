8df332a6f359277548c2322edce56eb5c3649eb0
jujomago <jujomago@gmail.com>
2015-10-11 18:56:04 -0400

    primer commit

---
+++ app/src/main/java/sinapsysit/com/thejobsproject/ListadoActivity.java
@@ -0,0 +1,13 @@
+package sinapsysit.com.thejobsproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado);
+    }
+}
